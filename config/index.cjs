cd1da0d46717a78005c3a6bb1a8c57613e3ea8a5
mark <37261842+wz930206@users.noreply.github.com>
2023-01-28 20:59:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ohPMX6s3u44LEv5rAi3CAZGVR0I8',
+      useTemplateId: 'o-40JZ4p4tjOVQITR4gABUIcKpXanFBIQ2lsejOXfVs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-01',
       festivals: [
