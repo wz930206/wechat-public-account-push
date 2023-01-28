cda0c11050b735f2e6b1e2ad35dee7045173683d
mark <37261842+wz930206@users.noreply.github.com>
2023-01-28 21:13:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,21 +23,18 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohPMX6s3u44LEv5rAi3CAZGVR0I8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o-40JZ4p4tjOVQITR4gABUIcKpXanFBIQ2lsejOXfVs',
+      useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-01',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝宝', year: '1998', date: '03-05',
+          type: '*生日', name: '宝宝', year: '1992', date: '03-05',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝宝', year: '1998', date: '04-01',
-        },
-        {
-          type: '纪念日', name: '第一次见面纪念日', year: '2023', date: '12-11',
-        },
+          type: '生日', name: '宝宝', year: '1992', date: '04-01',
+        }
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
@@ -51,13 +48,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'o-40JZ4p4tjOVQITR4gABUIcKpXanFBIQ2lsejOXfVs',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ohPMX6s3u44LEv5rAi3CAZGVR0I8',
+      id: '',
     }
   ],
 
